c86eb25cf059ba7c55d481387c70574ce707a49c
balexgtx <balexgtx@gmail.com>
2022-05-04 00:13:17 -0600

    Update rock_paper_scissors.py
    Validation for inputs added and exit function after the printing the results

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,28 +29,36 @@ def win_condition(user, computer):
 
 
 def game():
-    #We use random to select a random int between 1 and 3
+            #We use random to select a random int between 1 and 3
     computer_random = random.randint(1,3)
-    #We use our item_choice function to get the real item
+            #We use our item_choice function to get the real item
     computer_choice = item_choice(computer_random)
-    #We use our item_choice function to get the real item
-    user_choice = item_choice(int(input('''Selecciona que jugaras!: 
-    1. Piedra
-    2. Papel
-    3. Tijeras
-    ''')))
-    
-    if computer_choice == user_choice:
-        print('Ha sido un empate...!')
-    else:
-        result = win_condition(user_choice, computer_choice)
-        if result == True:
-            print('Feliciades usuario! has ganado! ')
-            print('usuario: ' + user_choice, ' Computadora: ', computer_choice)
-        else:
-            print('La computadora ha ganado! ')
-            print('usuario: ' + user_choice, ' Computadora: ', computer_choice)
-            
+            #We use our item_choice function to get the real item
+    while True:
+        try:
+            user_number = int(input('''Selecciona que jugaras!: 
+            1. Piedra
+            2. Papel
+            3. Tijeras
+            '''))
+            user_choice = item_choice(user_number)
+            if user_number <= 0 or user_number > 3:
+                raise ValueError
+            if computer_choice == user_choice:
+                print('Ha sido un empate...!')
+                exit()
+            else:
+                result = win_condition(user_choice, computer_choice)
+                if result == True:
+                    print('Feliciades usuario! has ganado! ')
+                    print('usuario: ' + user_choice, ' Computadora: ', computer_choice)
+                    exit()
+                else:
+                    print('La computadora ha ganado! ')
+                    print('usuario: ' + user_choice, ' Computadora: ', computer_choice)
+                    exit()
+        except ValueError:
+            print('Error ejejejjeje')
 
 def run():
     game()
