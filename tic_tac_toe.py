d7d2b06fe194ebc710a1aff8dbe8f19bd71a82e6
balexgtx <balexgtx@gmail.com>
2022-05-04 21:10:12 -0600

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,115 +1,75 @@
 import random
 
-def check_position(matriz,n,m):
-    if matriz[n][m] != "X" and matriz[n][m] != "O":
+
+def printBoard(board):
+    print(f'''
+         {board[0]} | {board[1]} | {board[2]}
+        -----------
+        
+         {board[3]} | {board[4]} | {board[5]}
+        -----------
+        
+         {board[6]} | {board[7]} | {board[8]}
+        ''')
+
+
+def check_position(board,pos):
+    if board[pos] != "X" or board[pos] != "O":
+        return True
+    else:
+        False
+
+
+def write_position(board, pos, ob):
+    if check_position(board,pos) == True:
+        board[pos] == ob
         return True
     else:
         return False
 
-#fila x columna
-def win_condition(matriz,option):
-    #fila x columna
-    #Test Horizontal
-    if matriz[0][0] == option and matriz[0][1] == option and matriz[0][2]  == option:
+
+def check_victory(board):
+    #Horizontal
+    if board[0] == board[1] and board[2] == board[1]:
         return True
-    elif matriz[1][0] == option and matriz[1][1] ==  option and matriz[1][2] == option:
+    elif board[3] == board[4] and board[5] == board[3]:
         return True
-    elif matriz[2][0] == option and matriz[2][1] == option and matriz[2][2] == option:
+    elif board[6] == board[7] and board[8] == board[6]:
         return True
-    #Test Vertical
-    elif matriz[0][0] == option and matriz[1][0] == option and matriz[2][0] == option:
+    #Vertical
+    elif board[0] == board[3] and board[6] == board[0]:
         return True
-    elif matriz[0][1] == option and matriz[1][1] == option and matriz[2][1] == option:
+    elif board[1] == board[4] and board[7] == board[1]:
         return True
-    elif matriz[0][2] == option and matriz[1][2] == option and matriz[2][2] == option:
+    elif board[2] == board[5] and board[8] == board[2]:
         return True
     #Diagonal
-    elif matriz[2][0] == option and matriz[1][1] == option and matriz[0][2] == option:
+    elif board[0] == board[4] and board[8] == board[0]:
         return True
-    elif matriz[0][0] == option and matriz[1][1] == option and matriz[2][2] == option:
+    elif board[6] == board[4] and board[2] == board[6]:
         return True
-    else: 
+    else:
         return False
-    
-
-def print_matriz(matriz):
-    print(f'''
-         {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]}
-        -----------
-        
-         {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]}
-        -----------
-        
-         {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]}
-        ''')
-
-
-def convert_choice_into_position(n):
-    if n == 1:
-        x = 0
-        y = 0
-        return x,y
-    elif n == 2:
-        x = 0
-        y = 1
-        return x,y
-    elif n == 3:
-        x = 0
-        y = 2
-        return x, y
-    elif n == 4:
-        x = 1
-        y = 0
-        return x, y
-    elif n == 5:
-        x = 1
-        y = 1
-        return x, y
-    elif n == 6:
-        x = 1
-        y = 2
-        return x, y
-    elif n == 7:
-        x = 2
-        y = 0
-        return x, y
-    elif n == 8:
-        x = 2
-        y = 1
-        return x, y
-    elif n == 0:
-        x = 2
-        y = 2
-        return x, y
-    
 
 
 def game():
-    matriz = [['1','2','3'],
-              ['4','5','6'],
-              ['7','8','9']]
-    #print(win_condition(matriz, 'X'))
-    #print(matriz[0][1], matriz[1][1], matriz[2][1])
-    #print(check_position(matriz, 0,0))
-    #print(matriz[0][0])
-    print('El juego ha comenzado!')
-        #computer_position_x = random.randint(0,2)
-    #print(computer_position_x)
-        #computer_position_y = random.randint(0,2)
-    #print(computer_position_y)
-    
-    #user_x = int(input('Ingresa la coordenada X: '))
-    #user_y = int(input('Ingresa la coordenada Y: '))
-    #print(print_matriz(matriz))
-    print_matriz(matriz)
-    n = int(input('Ingresa el numero al que quieres mover tu ficha!: '))
-    a = convert_choice_into_position(n)
-    print(matriz[a[0]][a[1]])
+    board = ['1','2','3','4','5','6','7','8', '9']
+    #print(board)
+    print('Juego iniciado!...')
+    print('El usuario coloca X!')
+    print('La computadora juega con O!')
+    printBoard(board)
+    #print(check_victory(board))
+    while check_victory != True:
+        try:
+            print('Donde quieres colocar X!: ')
+            user_movement = int(input())
+        except ValueError:
+            print('Ingresa datos validos! ')
+    else:
+        pass
     
     
-    
-    
-
 def run():
     game()
 
